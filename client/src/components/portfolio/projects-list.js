e1bd5e13413b918170e6d107e6797dd1bd705c22
Dylan Travis <dylan.travis23@outlook.com>
2022-03-09 20:44:38 -0800

    project list grid set up

--- client/src/components/portfolio/projects-list.js
+++ client/src/components/portfolio/projects-list.js
@@ -1,9 +1,26 @@
-function ProjectList() {
+import classes from "./projects-list.module.css";
+import Project from "./project";
+
+function ProjectList(props) {
+  const { projects, filter } = props;
+
   return (
-    <div>
-      <p>Showing x projects filtered by xxxx </p>
+    <div className={classes.container}>
+      {filter === "Show All" ? (
+        <p>
+          Showing all projects. Use the filter to list them by skill or
+          technology.
+        </p>
+      ) : (
+        <p>Showing {projects.length} projects filtered by {filter} </p>
+      )}
+      <ul className={classes.list}>
+        {projects.map((prj) => (
+          <li key={prj.title}><Project project={prj}/></li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default ProjectList;
