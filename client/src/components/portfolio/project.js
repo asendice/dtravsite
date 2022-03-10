475a19072e9983c177db96d39343ed801d149d4b
Dylan Travis <dylan.travis23@outlook.com>
2022-03-10 14:24:59 -0800

    overlay of skills

--- client/src/components/portfolio/project.js
+++ client/src/components/portfolio/project.js
@@ -1,4 +1,5 @@
 import classes from "./project.module.css";
+import Tag from "../ui/tag";
 
 function Project(props) {
   const { name, description, images, skills } = props.project;
@@ -10,11 +11,22 @@ function Project(props) {
         <div className={`${classes.dot} ${classes.green}`}></div>
         <h5>{name}</h5>
       </div>
-      <div className={classes.content} style={{backgroundImage: `url(${images[0]})`}}>
-
+      <div
+        className={classes.content}
+        style={{ backgroundImage: `url(${images[0]})` }}
+      >
+        <div className={classes.overlay}>
+          <ul className={classes.tagList}>
+            {skills.map((skill, index) => (
+              <li key={index}>
+                <Tag title={skill} />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Project;
