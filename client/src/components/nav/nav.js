e2594e3a340a47fce257c9e6ea1eda8a685f74de
Dylan Travis <dylan.travis23@outlook.com>
2022-03-11 00:28:51 -0800

    changed Link components to NavLinks from react-router

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -1,38 +1,38 @@
 import classes from "./nav.module.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Nav(props) {
   return (
     <div className={classes.container}>
       <ul className={classes.list}>
-        <Link
+        <NavLink
           className={classes.listItem}
           to="/"
           onClick={() => props.setOpen(false)}
         >
           <li>about</li>
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           className={classes.listItem}
           to="/portfolio"
           onClick={() => props.setOpen(false)}
         >
           <li>portfolio</li>
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           className={classes.listItem}
           to="/technologies"
           onClick={() => props.setOpen(false)}
         >
           <li>tech</li>
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           className={classes.listItem}
           to="/contact"
           onClick={() => props.setOpen(false)}
         >
           <li>contact</li>
-        </Link>
+        </NavLink>
       </ul>
     </div>
   );
