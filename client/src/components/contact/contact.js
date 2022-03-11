5a394ed398fd6ac0dafbc39c79b19018759167f8
Dylan Travis <dylan.travis23@outlook.com>
2022-03-10 19:21:36 -0800

    contact form ui

--- client/src/components/contact/contact.js
+++ client/src/components/contact/contact.js
@@ -1,12 +1,32 @@
-function Contact() {
+import classes from "./contact.module.css";
+import { BsFillPersonFill, BsFillEnvelopeFill } from "react-icons/bs";
+import Button from "../ui/button";
 
+function Contact() {
   return (
-    <div>
+    <div className={classes.container}>
       <h1>Get in touch</h1>
-      <p>If you want to get in touch, talk tome about a project collaboration or just say hello,</p>
-      <p>fill out this form below or send an email to <a>dylantravisdev@gmail.com</a> and let's talk!</p>
+      <p>
+        If you want to get in touch, talk tome about a project collaboration or
+        just say hello, fill out this form below or send an email to{" "}
+        <a className={classes.link}>dylantravisdev@gmail.com</a> and let's talk!
+      </p>
+
+      <form className={classes.form}>
+        <div className={classes.inputContainer}>
+          <BsFillPersonFill className={classes.icon} />
+          <input placeholder="Fill out your name" />
+        </div>
+        <div className={classes.inputContainer}>
+          <BsFillEnvelopeFill className={classes.icon} />
+          <input placeholder="Write a message" />
+        </div>
+        <div className={classes.btnContainer}>
+          <Button title="SUBMIT YOUR MESSAGE" />
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Contact;
