e26dc4708a855f44442f83af86315c74477d3dec
Dylan Travis <dylan.travis23@outlook.com>
2022-03-15 10:09:07 -0700

    project buttons

--- client/src/components/project/project.js
+++ client/src/components/project/project.js
@@ -1,11 +1,32 @@
 import classes from "./project.module.css";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import backendApi from "../../apis/backendApi";
+import Button from "../ui/button";
+import { BsBoxArrowUpRight, BsLinkedin, BsGithub, BsFillFilePdfFill } from "react-icons/bs";
 
-
-function Project(props) {
-
+function Project() {
+  const [project, setProject] = useState({});
   const { name } = useParams();
-  const project = props.projects.find((prj) => prj.name === name);
+
+  async function getProjectByName() {
+    await backendApi
+      .get(`/projects/${name}`)
+      .then((response) => {
+        if (response) {
+          return response;
+        } else {
+          const error = new Error(`Error ${error.status}: ${error.statusText}`);
+          error.response = response;
+          throw error;
+        }
+      })
+      .then((response) => setProject(response.data.message[0]));
+  }
+
+  useEffect(() => {
+    getProjectByName();
+  }, []);
 
   return (
     <div className={classes.container}>
@@ -13,9 +34,26 @@ function Project(props) {
         <h1>{project.name}</h1>
         <p>{project.excerpt}</p>
       </div>
-      {name}
+
+      <div className={classes.btnContainer}>
+        <Button color="green">
+          {" "}
+          <BsBoxArrowUpRight className={classes.icon} /> <p className={classes.btnText}>Visit the website</p>
+        </Button>
+        <div className={classes.socials}>
+          <Button color="linkedin">
+            <BsLinkedin className={classes.icon}  />{" "}
+          </Button>
+          <Button color="github">
+            <BsGithub className={classes.icon}  />
+          </Button>
+          <Button color="red">
+            <BsFillFilePdfFill className={classes.icon}  />
+          </Button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Project;
