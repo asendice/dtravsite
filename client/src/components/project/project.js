f54408601d19c0a7d98f96013f598705c25c18d2
Dylan Travis <dylan.travis23@outlook.com>
2022-03-15 10:30:41 -0700

    added links to project buttons

--- client/src/components/project/project.js
+++ client/src/components/project/project.js
@@ -3,11 +3,18 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import backendApi from "../../apis/backendApi";
 import Button from "../ui/button";
-import { BsBoxArrowUpRight, BsLinkedin, BsGithub, BsFillFilePdfFill } from "react-icons/bs";
+import {
+  BsBoxArrowUpRight,
+  BsLinkedin,
+  BsGithub,
+  BsFillFilePdfFill,
+} from "react-icons/bs";
 
 function Project() {
   const [project, setProject] = useState({});
   const { name } = useParams();
+
+  console.log(project);
 
   async function getProjectByName() {
     await backendApi
@@ -36,19 +43,31 @@ function Project() {
       </div>
 
       <div className={classes.btnContainer}>
-        <Button color="green">
-          {" "}
-          <BsBoxArrowUpRight className={classes.icon} /> <p className={classes.btnText}>Visit the website</p>
+        {project.live && (
+          <Button color="green" outsideLink={`${project.live}`}>
+            <BsBoxArrowUpRight className={classes.icon} />{" "}
+            <p className={classes.btnText}>Visit the website</p>
+          </Button>
+        )}
+        <Button
+          color="github"
+          outsideLink={`${project.gitHub}`}
+        >
+          <BsBoxArrowUpRight className={classes.icon} />{" "}
+          <p className={classes.btnText}>Visit the github</p>
         </Button>
         <div className={classes.socials}>
           <Button color="linkedin">
-            <BsLinkedin className={classes.icon}  />{" "}
+            <BsLinkedin className={classes.icon} />{" "}
           </Button>
-          <Button color="github">
-            <BsGithub className={classes.icon}  />
+          <Button color="github" outsideLink="https://github.com/asendice">
+            <BsGithub className={classes.icon} />
           </Button>
-          <Button color="red">
-            <BsFillFilePdfFill className={classes.icon}  />
+          <Button
+            color="red"
+            outsideLink="https://dtravmysite.s3.us-west-1.amazonaws.com/Dylan_Travis_-_Web_Developer.pdf"
+          >
+            <BsFillFilePdfFill className={classes.icon} />
           </Button>
         </div>
       </div>
