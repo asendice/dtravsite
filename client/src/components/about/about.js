2d4bb99988b8670f3db590a35fc6caa12ef7fd50
Dylan Travis <dylan.travis23@outlook.com>
2022-03-17 13:52:50 -0700

    added breaks to about paragraph

--- client/src/components/about/about.js
+++ client/src/components/about/about.js
@@ -14,18 +14,20 @@ function About() {
         <p>
           I am a full-stack web developer specializing in React and
           React-Native.
-        </p>
-        <p>
-          I am currently seeking a remote Front-End || Full-Stack web developer
-          position.{" "}
-        </p>
-        <p>
+          <br />I am currently seeking a remote Front-End || Full-Stack web
+          developer position. <br />
           Feel free to take a look at my latest projects on the{" "}
-          <Link className={classes.link} to="/portfolio">web portfolio page</Link>.
-        </p>
-        <p>
+          <Link className={classes.link} to="/portfolio">
+            web portfolio page
+          </Link>
+          .
+          <br />
           The technologies that I have used and or actively using can be view on
-          the <Link className={classes.link} to="/technologies">tech stack page</Link>.
+          the{" "}
+          <Link className={classes.link} to="/technologies">
+            tech stack page
+          </Link>
+          .
         </p>
       </div>
       <div className={classes.btnContainer}>
