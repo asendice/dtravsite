505c314892a190574b0c617cf817731047b9526c
Dylan Travis <dylan.travis23@outlook.com>
2022-03-28 10:05:39 -0700

    active nav bkg color change

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -1,33 +1,37 @@
 import classes from "./nav.module.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 function Nav(props) {
+
+  const { pathname } = useLocation();
+  console.log(pathname)
+
   return (
     <div className={classes.container}>
       <ul className={classes.list}>
         <NavLink
-          className={classes.listItem}
+          className={`${classes.listItem} ${pathname === "/" ? classes.active : "" }`}
           to="/"
           onClick={() => props.setOpen(false)}
         >
           <li>about</li>
         </NavLink>
         <NavLink
-          className={classes.listItem}
+          className={`${classes.listItem} ${pathname.includes("/portfolio") ? classes.active : "" }`}
           to="/portfolio"
           onClick={() => props.setOpen(false)}
         >
           <li>portfolio</li>
         </NavLink>
         <NavLink
-          className={classes.listItem}
+          className={`${classes.listItem} ${pathname === "/technologies" ? classes.active : "" }`}
           to="/technologies"
           onClick={() => props.setOpen(false)}
         >
           <li>tech</li>
         </NavLink>
         <NavLink
-          className={classes.listItem}
+          className={`${classes.listItem} ${pathname === "/contact" ? classes.active : "" }`}
           to="/contact"
           onClick={() => props.setOpen(false)}
         >
