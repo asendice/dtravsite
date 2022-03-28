69acd0a501a1ef15a76f13395cedfa6d36f486f6
Dylan Travis <dylan.travis23@outlook.com>
2022-03-28 14:17:02 -0700

    square mobile button

--- client/src/components/nav/squares.js
+++ client/src/components/nav/squares.js
@@ -1,21 +1,22 @@
 import classes from "./squares.module.css";
 import { Link } from "react-router-dom";
 
-function Squares(){
-
+function Squares() {
   return (
-    <Link to="/portfolio" className={classes.bigSquare}>
-      <div className={`${classes.square} ${classes.one}`}></div>
-      <div className={`${classes.square} ${classes.two}`}></div>
-      <div className={`${classes.square} ${classes.three}`}></div>
-      <div className={`${classes.square} ${classes.four}`}></div>
-      <div className={`${classes.square} ${classes.five}`}></div>
-      <div className={`${classes.square} ${classes.six}`}></div>
-      <div className={`${classes.square} ${classes.seven}`}></div>
-      <div className={`${classes.square} ${classes.eight}`}></div>
-      <div className={`${classes.square} ${classes.nine}`}></div>
+    <Link to="/portfolio" className={classes.container}>
+      <div className={classes.bigSquare}>
+        <div className={`${classes.square} ${classes.one}`}></div>
+        <div className={`${classes.square} ${classes.two}`}></div>
+        <div className={`${classes.square} ${classes.three}`}></div>
+        <div className={`${classes.square} ${classes.four}`}></div>
+        <div className={`${classes.square} ${classes.five}`}></div>
+        <div className={`${classes.square} ${classes.six}`}></div>
+        <div className={`${classes.square} ${classes.seven}`}></div>
+        <div className={`${classes.square} ${classes.eight}`}></div>
+        <div className={`${classes.square} ${classes.nine}`}></div>
+      </div>
     </Link>
-  )
+  );
 }
 
 export default Squares;
