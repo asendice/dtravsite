fcbd97ba98ddb8ff2f75e84335cf083f6c44d80d
Dylan Travis <dylan.travis23@outlook.com>
2022-04-03 18:01:08 -0700

    banner hover

--- client/src/components/ui/banner.js
+++ client/src/components/ui/banner.js
@@ -1,16 +1,19 @@
 import classes from "./banner.module.css";
 
-function Banner(props){
-  const {title} = props;
+function Banner(props) {
+  const { title } = props;
   return (
     <div className={classes.container}>
-    <a className={classes.banner}>
-      {title}
-    </a>
-
+      <a
+        href="https://oneclick-dt.herokuapp.com"
+        rel="noopener noreferrer"
+        target="_blank"
+        className={classes.banner}
+      >
+        {title}
+      </a>
     </div>
-  )
-
+  );
 }
 
 export default Banner;
